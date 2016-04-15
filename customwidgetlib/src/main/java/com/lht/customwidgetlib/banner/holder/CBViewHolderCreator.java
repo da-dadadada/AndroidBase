051fd171d03f4b3f3d50b83b59a1326dcd60423b
jlan <jlan@landhightech.com>
2016-04-15 14:33:23 +0800

    add banner

---
+++ customwidgetlib/src/main/java/com/lht/customwidgetlib/banner/holder/CBViewHolderCreator.java
@@ -0,0 +1,10 @@
+package com.lht.customwidgetlib.banner.holder;
+/**
+ * @ClassName :  ViewHolderCreator 
+ * @Description : 
+ * @Author Sai
+ * @Date 2014å¹?1æœ?0æ—?ä¸‹åˆ3:29:34
+ */
+public interface CBViewHolderCreator<Holder> {
+	public Holder createHolder();
+}
